e0d666d916c922f771fec158e3d5d498542dc316
dgulacsy <dominik.gulacsy@gmail.com>
2025-12-01 10:57:07 +0000

    fix job source code

--- src/default-sdp/transformations/departure_delay.sql
+++ src/default-sdp/transformations/departure_delay.sql
@@ -1,6 +1,8 @@
 CREATE OR REFRESH MATERIALIZED VIEW departure_delay AS
 SELECT
-    SUM(dep_delay) AS total_delay,
-    EXTRACT(YEAR FROM dep_timestamp) AS year,
-    EXTRACT(MONTH FROM dep_timestamp) AS month
-FROM demo_wzz_dbxapps_default.default.flights_small;
+    origin,
+    dest,
+    month,
+    SUM(dep_delay) AS total_delay
+FROM demo_wzz_dbxapps_default.default.flights_2013 
+GROUP BY origin, dest, month;
